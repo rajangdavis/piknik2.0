7b47e433003dd1a9e4219f35e57d95ab82fa563f
elikagan <eli.kagan@gmail.com>
2014-11-25 21:51:57 -0800

    fixed stream creation

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,22 +1,47 @@
 class StreamsController < ApplicationController
+
   def new
+    @stream = Stream.new
+    create
   end
-
-  def create
-  end
-
+  
   def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @stream = Stream.find(params[:id])
   end
 
   def index
+    @stream = Stream.find(params[:id])
+    redirect_to edit_stream_path(:stream)
   end
 
-  def show
+  def update
+    
+    @stream = Stream.find(params[:id])
+    if @stream.update_attributes(stream_params)
+      redirect_to user_path(@stream.user_id)
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @user_id = current_user.id
+    @stream = Stream.new(params[:user_id])
+    if @stream.save
+      redirect_to edit_stream_path(@stream)
+    else
+      render 'index'
+    end
+  end
+
+  def destroy
+    @stream = Stream.find(params[:id])
+    @stream.destroy
+  end
+  
+  private
+
+  def stream_params
+    params.require(:stream).permit(:user_id, :name)
   end
 end
