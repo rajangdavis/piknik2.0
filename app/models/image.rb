ec4d638338f7fde34428dd222c5510a4d3c4b701
rajangdavis <rajangdavis@gmail.com>
2014-11-26 13:50:54 -0800

    updated image info stuff in the image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,8 +5,8 @@ class Image < ActiveRecord::Base
 has_attached_file :image, styles: {
         :small    => ['900x900^', :jpg] 
     }
-    :convert_options { :small => '-gravity center -crop 900x900+0+0 -quality 20%' }
-validates_attachment_content_type :image, content_type: => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    # :convert_options { :small => '-gravity center -crop 900x900+0+0 -quality 20%' }
+validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   	
 
 
