3b99ae1866544de3e82b640f64799812ddad2798
rajangdavis <rajangdavis@gmail.com>
2014-11-26 13:33:58 -0800

    maybe fixed some imagemacgick?

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,9 +4,9 @@ class Image < ActiveRecord::Base
 
 	:styles => {
         :small    => ['900x900^', :jpg] 
-    },
+    }
     :convert_options => { :small => '-gravity center -crop 900x900+0+0 -quality 20%' }
-	validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   	
 
 
